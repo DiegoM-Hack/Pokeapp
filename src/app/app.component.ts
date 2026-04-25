594c7db007a15b913e2d9173ca0b39452cc7251f
DiegoM-Hack <santitodiego12@gmail.com>
2026-04-25 16:08:09 -0500

    horientacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor(private platform: Platform) {
+    this.initApp();
+  }
+
+  async initApp() {
+    await this.platform.ready();
+
+    try {
+      await ScreenOrientation.lock({ orientation: 'landscape' });
+    } catch (error) {
+      console.log('Error al bloquear orientación:', error);
+    }
+  }
 }
